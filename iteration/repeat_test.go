331b48eeb1435939d87fe06c5d0563e615fcc318
shudiptod <shudiptod@gmail.com>
2024-10-25 03:10:35 +0600

    Completed: iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,21 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+		repeatedString:= Repeat("a", 6)
+		fmt.Println(repeatedString)
+		// Output: aaaaaa
+}
